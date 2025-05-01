628a15e27045d87c9d9b34947c8aa0f997c2202a
LauraSchiavon <lauraschiavon00@gmail.com>
2025-05-01 17:37:36 -0300

    link telefone

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -9,58 +9,63 @@ const Contact: React.FC = () => {
     <section id="contato" className="py-20 bg-black text-white">
       <div className="container max-w-7xl mx-auto px-4">
         <div className="text-center mb-16">
-          <h2 className="text-4xl text-white font-bold">Fale Conosco</h2>
-          <p className="text-white mt-4">
+          <h2 className="text-4xl font-bold">Fale Conosco</h2>
+          <p className="mt-4">
             Entre em contato com nossa equipe para tirar dúvidas ou agendar sua
             vistoria.
           </p>
         </div>
 
-        {/* Blocos de contato lado a lado */}
         <div className="flex flex-wrap justify-between gap-6">
-          {/* Item */}
-          <div className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl p-6 hover:border-white transition-colors">
+          {/* Telefone */}
+          <a
+            href="tel:+5519974086574"
+            className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl p-6 hover:border-white transition-colors block"
+          >
             <div className="flex items-center space-x-4 mb-3">
               <Phone size={28} className="text-white" />
-              <h3 className="text-xl font-semibold  text-white">Telefone</h3>
+              <h3 className="text-xl font-semibold">Telefone</h3>
             </div>
-            <a href="tel:+5519974086574" className="text-white">
-              (19) 97408-6574
-            </a>
-          </div>
+            <p>(19) 97408-6574</p>
+          </a>
 
-          <div className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl py-6 pr-12 pl-6 hover:border-white transition-colors">
-            <div className="flex items-center space-x-4 mb-3 ">
+          {/* E-mail */}
+          <a
+            href="mailto:vistorivistoriapiracicaba@gmail.com"
+            className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl py-6 pl-6 pr-12 hover:border-white transition-colors block"
+          >
+            <div className="flex items-center space-x-4 mb-3">
               <Mail size={28} className="text-white" />
               <h3 className="text-xl font-semibold">E-mail</h3>
             </div>
-            <a
-              href="mailto:contato@vistoripiracicaba.com.br"
-              className="text-white "
-            >
-              vistorivistoriapiracicaba@gmail.com
-            </a>
-          </div>
+            <p>vistorivistoriapiracicaba@gmail.com</p>
+          </a>
 
-          <div className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl p-6 hover:border-white transition-colors">
+          {/* Endereço */}
+          <a
+            href="https://www.google.com.br/maps/place/Av.+Rio+das+Pedras,+726+-+Piracicamirim,+Piracicaba+-+SP,+13420-590"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl p-6 hover:border-white transition-colors block"
+          >
             <div className="flex items-center space-x-4 mb-3">
               <MapPin size={28} className="text-white" />
               <h3 className="text-xl font-semibold">Endereço</h3>
             </div>
-            <a
-              href="https://www.google.com.br/maps/place/Av.+Rio+das+Pedras,+726+-+Piracicamirim,+Piracicaba+-+SP,+13420-590/@-22.7431051,-47.627969,17z/data=!3m1!4b1!4m6!3m5!1s0x94c6303886816db3:0x58c4924d82d90d68!8m2!3d-22.7431101!4d-47.6253941!16s%2Fg%2F11cnd8hn2t?entry=ttu&g_ep=EgoyMDI1MDQyOC4wIKXMDSoASAFQAw%3D%3D"
-              className="text-white"
-            >
-              Avenida Rio das Pedras, 726, Piracicaba/SP, CEP: 13.425- 380
-            </a>
-          </div>
+            <p>
+              Av. Rio das Pedras, 726, Piracicaba/SP
+              <br />
+              CEP: 13.425-380
+            </p>
+          </a>
 
+          {/* Horário (não precisa ser link) */}
           <div className="flex-1 min-w-[230px] bg-black border border-gray-800 rounded-xl p-6 hover:border-white transition-colors">
             <div className="flex items-center space-x-4 mb-3">
               <Clock size={28} className="text-white" />
               <h3 className="text-xl font-semibold">Horário</h3>
             </div>
-            <p className="text-white">
+            <p>
               Seg-Sex: 8h às 18h
               <br />
               Sáb: 8h às 12h
